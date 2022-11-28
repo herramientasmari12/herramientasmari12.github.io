d547a46bcfd3b3a252182bbe63243e412cf93ac9
herramientasmari12 <118775866+herramientasmari12@users.noreply.github.com>
2022-11-27 23:46:17 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyBKoxILafX7J7fkaLa8zAL8JUjO9kwFIFk",
+  authDomain: "herramientasmari121.firebaseapp.com",
+  projectId: "herramientasmari121",
+  storageBucket: "herramientasmari121.appspot.com",
+  messagingSenderId: "146707967003",
+  appId: "1:146707967003:web:9d24c80494c929b638dcbe",
+  measurementId: "G-YBQGBHJ5DF"
 });
